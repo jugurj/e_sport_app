55f420072bb559ab2ff4726001d7211d96e8876d
jugurj <juriyunna27@gmail.com>
2019-05-18 17:47:41 +0300

    Created Enroll component for Promotion section. Implemented FormField component to create input fields by given props configurations. Created user email enroll field.

--- src/components/home/promotion/Promotion.js
+++ src/components/home/promotion/Promotion.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PromotionTitle from './PromotionTitle';
+import Enroll from './Enroll';
 
 const Promotion = () => {
     return (
         <div className="promotion_wrapper">
             <div className="container">
                 <PromotionTitle/>
+                <Enroll/>
             </div>
         </div>
     );
