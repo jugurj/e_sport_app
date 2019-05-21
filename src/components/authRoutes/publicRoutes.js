7442ff1f41b57d5513dc1ef16eb5cac19e5226ac
jugurj <juriyunna27@gmail.com>
2019-05-21 15:48:05 +0300

    Separated public routes into separate component as well as private routes. Added restricted prop to the public routes so that autorizated users can't get access to sign in page until they log out.

---
+++ src/components/authRoutes/publicRoutes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PublicRoutes = ({
+    user,
+    component: Component,
+    ...rest
+}) => {
+    return <Route {...rest} component={(props) => (
+        rest.restricted ? 
+            (user ?
+                <Redirect to="/dashboard"/>
+                : <Component {...props} user={user}/>
+            )
+            : <Component {...props} user={user}/>
+    )}/>
+};
+
+export default PublicRoutes;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Layout from './hoc/Layout';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-import PrivateRoutes from './components/authRoutes/privateRoutes';
+import PrivateRoute from './components/authRoutes/privateRoutes';
+import PublicRoute from './components/authRoutes/publicRoutes';
 
 import Home from './components/home/Home';
 import SignIn from './components/authorization/SignIn';
@@ -13,10 +14,9 @@ const Routes = (props) => {
   return (
     <Layout>
       <Switch>
-        <PrivateRoutes {...props} path='/dashboard' exact component={Dashboard}/>
-        {/* <Route exact component={Dashboard} path="/dashboard"/> */}
-        <Route exact component={SignIn} path="/sign_in"/>
-        <Route exact component={Home} path="/"/>
+        <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
+        <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
+        <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
       </Switch>
     </Layout>
   );
